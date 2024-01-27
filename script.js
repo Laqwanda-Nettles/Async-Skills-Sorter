5cb239e4fe02729a10b1fc29bda61e8d2a5f6f8d
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-01-26 22:04:49 -0600

    created json, html, + js files. created an async function to fetch data.

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+async function skills() {
+  try {
+    const data = await fetch("skills.json");
+    const newData = await data.json();
+    console.log(newData);
+
+    const sortLevel = newData.sort((a, b) => (a.level > b.level ? 1 : -1));
+    console.log(sortLevel);
+  } catch (error) {
+    console.error(error);
+  }
+}
+skills();
