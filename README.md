082d20f26050d8bc38909d564f1939f0436d23ef
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-01-26 22:17:15 -0600

    edited readme, html , + js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Async-Skills-Sorter
+
+## Objective
+
+Develop an application that asynchronously fetches a list of skills with associated skill levels and sorts them based on their proficiency level.
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function skills() {
     const newData = await data.json();
     console.log(newData);
 
-    const sortLevel = newData.sort((a, b) => (a.level > b.level ? 1 : -1));
+    const sortLevel = newData.sort((a, b) => (a.level < b.level ? 1 : -1));
     console.log(sortLevel);
   } catch (error) {
     console.error(error);
